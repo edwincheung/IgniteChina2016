7356b19c64d387e3ce41dc238e0462a1ed06218f
Edwin Cheung <edwin.cheung@miriamedwin.com>
2016-11-25 11:57:28 +0800

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,11 +11,11 @@ namespace WebApplication.Controllers
     {
         public IActionResult Index()
         {
-            //string nameFX = PlatformServices.Default.Application.RuntimeFramework.FullName.ToString();
-            //string osNameAndVersion = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            string nameFX = PlatformServices.Default.Application.RuntimeFramework.FullName.ToString();
+            string osNameAndVersion = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
             
             //ViewData["Message"] = " ";
-            //ViewData["Message"] = nameFX + " running on " + osNameAndVersion + ". " ;
+            ViewData["Message"] = nameFX + " running on " + osNameAndVersion + ". " ;
             
             return View();
         }
