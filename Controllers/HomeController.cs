dc5c4303bfc0dcd2a36e641cc9b4318a2563333d
Edwin Cheung <edwin.cheung@miriamedwin.com>
2016-11-29 15:23:46 +0800

    upated

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,7 +14,6 @@ namespace WebApplication.Controllers
             string nameFX = PlatformServices.Default.Application.RuntimeFramework.FullName.ToString();
             string osNameAndVersion = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
                
-            //ViewData["Message"] = " ";
             ViewData["Greeting"] = "";
             DateTime today = DateTime.Today.ToLocalTime();
             ViewData["Today"] = today.Year.ToString() + "年" + today.Month.ToString() + "月" + today.Day.ToString() + "日"; 
