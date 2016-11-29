4f97d461137455367647c17becc1cb2c6eb22e51
Edwin Cheung <edwin.cheung@miriamedwin.com>
2016-11-29 15:20:00 +0800

    reason

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication.Controllers
             string osNameAndVersion = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
                
             //ViewData["Message"] = " ";
-            ViewData["Greeting"] = "Ignite China 2016";
+            ViewData["Greeting"] = "";
             DateTime today = DateTime.Today.ToLocalTime();
             ViewData["Today"] = today.Year.ToString() + "年" + today.Month.ToString() + "月" + today.Day.ToString() + "日"; 
             ViewData["DOTNETCORE"] = nameFX;
